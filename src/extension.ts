8a146b6178cc45180a0325c7fee69a9f52ec9d8b
André Kugland <kugland@gmail.com>
2020-01-21 04:15:07 -0300

    updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,27 @@
+//  A plugin that allows typing polytonic Greek into VS Code.
+//  Copyright (C) 2020  André Kugland
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 'use strict';
 
 import * as vscode from 'vscode';
 
+/**
+ * Mapping of BetaCode codes to Greek Unicode characters.
+ */
 const betaCodeMap: Record<string, string> = {
   // Uppercase characters
   A: 'Α', B: 'Β', C: 'Ξ', D: 'Δ', E: 'Ε', F: 'Φ', G: 'Γ', H: 'Η',
@@ -24,19 +44,36 @@ const betaCodeMap: Record<string, string> = {
   '|': '\u0345',  // ypogegrammeni (iota subscript)
   '+': '\u0308',  // dialytika (diaeresis)
   '_': '\u0304',  // macron
-  "^": '\u0306',  // vrachy (breve)
+  '^': '\u0306',  // vrachy (breve)
+
+  // Misc
+  ':': '·',
 };
 
-const betaCodeReplaces = Object.keys(betaCodeMap)
-  .sort((a, b) => b.length - a.length)
-  .map(code => [code, betaCodeMap[code]]);
+/**
+ * betaCodeMap represented as an array of tuples [code, greek],
+ * ordered by code length (needed to ensure correct translating
+ * of codes with more than one character).
+ */
+const betaCodeReplaces: [string, string][] = (
+  Object.keys(betaCodeMap)
+    .sort((a, b) => b.length - a.length)
+    .map(code => [code, betaCodeMap[code]])
+);
 
+/**
+ * Precedence of diacriticals (used in normalizeDiacriticals).
+ */
 const diacriticalPrecedence: Record<string, number> = {
   '(': 0, ')': 0, '+': 1,
   '/': 2, '\\': 2, '=': 2,
   '|': 3, '_': 4, '^': 4,
 };
 
+/**
+ * Reorder diacriticals or String.normalize to be able to compose
+ * combined characters whenever possible.
+ */
 const normalizeDiacriticals = (text: string) => {
   return text.replace(/([)(\\/=|_^+]{2,})/g, (match: string) => {
     const normalized = match.split('')
@@ -46,32 +83,41 @@ const normalizeDiacriticals = (text: string) => {
   });
 }
 
+/**
+ * Main function, which does the actual translation.
+ *
+ * @param text   text being translated.
+ */
 const translateBetaCode = (text: string) => {
   let result = text;
+  // First reorder codes for diacriticals when needed.
   result = normalizeDiacriticals(text);
+  // Then replace codes with Greek unicode characters.
   for (const [code, greek] of betaCodeReplaces) {
     result = result.split(code).join(greek);
   }
-  return result.normalize('NFKC');
+  // Normalize the result to combined Unicode characters.
+  result = result.normalize('NFKC');
+  return result;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.reverseWord', function () {
-		const editor = vscode.window.activeTextEditor;
+  let disposable = vscode.commands.registerCommand('extension.betaCode', function () {
+    const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			const document = editor.document;
-			const selections = editor.selections;
-	
-			editor.edit(editBuilder => {
+    if (editor) {
+      const document = editor.document;
+      const selections = editor.selections;
+
+      editor.edit(editBuilder => {
         for (const selection of selections) {
-          const paragraph = document.getText(selection);
-          const translated = translateBetaCode(paragraph);
-          editBuilder.replace(selection, translated);
+          const text = document.getText(selection);
+          const translatedText = translateBetaCode(text);
+          editBuilder.replace(selection, translatedText);
         }
-			});
-		}
-	});
+      });
+    }
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
